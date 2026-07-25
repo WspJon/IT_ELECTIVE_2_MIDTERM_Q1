b106155890dd7427a4b7dc3bcef34a31accaf436
EYOW <realesjonnidel@gmail.com>
2026-07-25 09:59:40 +0800

    Magic Cards

--- Reales/Reales/Models/MagicCard.cs
+++ Reales/Reales/Models/MagicCard.cs
@@ -1,6 +1,14 @@
-﻿namespace Reales.Models
+﻿namespace MagicCardApp.Models
 {
     public class MagicCard
     {
+        public string Name { get; set; }
+        public string TypeLine { get; set; }
+        public string ImageUrl { get; set; }
+        public string OracleText { get; set; }
+        public string FlavorText { get; set; }
+        public string PowerToughness { get; set; }
+        public string Set { get; set; }
+        public string Rarity { get; set; }
     }
 }
