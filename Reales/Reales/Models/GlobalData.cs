fa7c6786a9c49c6562111450bebcd8803233af7e
EYOW <realesjonnidel@gmail.com>
2026-07-25 09:59:24 +0800

    GlobalData

---
+++ Reales/Reales/Models/GlobalData.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicCardApp.Models
+{
+    public static class GlobalData
+    {
+        // Ito ang magche-check kung naka-login na ang user o hindi
+        public static bool IsLoggedIn { get; set; } = false;
+    }
+}
