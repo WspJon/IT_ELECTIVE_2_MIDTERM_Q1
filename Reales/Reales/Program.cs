1144de49cf53869a4a5d804f10cde66b647601f5
EYOW <realesjonnidel@gmail.com>
2026-07-25 09:59:57 +0800

    Program.cs

--- Reales/Reales/Program.cs
+++ Reales/Reales/Program.cs
@@ -21,7 +21,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+ name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
